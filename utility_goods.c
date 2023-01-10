395a6d22a07517fdbb6763a3d2cca1b6e4e5b10b
Nicolas <stetco.nicolas.t@gmail.com>
2023-01-10 12:23:14 +0100

    gestione ExpiringDate

--- utility_goods.c
+++ utility_goods.c
@@ -14,7 +14,6 @@ goods generateGoods(int type){
     goods g;
     struct timespec t;
 
-    clock_gettime(CLOCK_REALTIME, &g.generationTime);
     clock_gettime(CLOCK_REALTIME, &t);
     g.type=type;
     g.booked=0;
@@ -22,7 +21,13 @@ goods generateGoods(int type){
     g.dimension= (t.tv_nsec%SO_SIZE)+1;
     g.state=in_port;
     clock_gettime(CLOCK_REALTIME, &t);
-    g.lifeTime=(t.tv_nsec%(SO_MAX_VITA-SO_MIN_VITA))+SO_MIN_VITA+1;
+
+    if (SO_MIN_VITA == SO_MAX_VITA) 
+        t.tv_sec+= SO_MIN_VITA;
+    else    
+        t.tv_sec+=(t.tv_nsec%(SO_MAX_VITA-SO_MIN_VITA))+SO_MIN_VITA+1;
+
+    g.expiringDate=t;
 
     return g;
 }
@@ -33,32 +38,32 @@ int isExpired(goods g){
     struct timespec now;
     
     clock_gettime(CLOCK_REALTIME, &now);
-    
-    if(now.tv_sec-g.generationTime.tv_sec > g.lifeTime ) return 0;
-    else if (now.tv_sec-g.generationTime.tv_sec == g.lifeTime){
-        if(now.tv_nsec >= g.generationTime.tv_nsec) return 1;
-        else return 0;
-    }else return 1;
+
+    if(now.tv_sec>g.expiringDate.tv_sec) return 1;
+    else if (now.tv_sec<g.expiringDate.tv_sec) return 0;
+    else if (now.tv_nsec> g.expiringDate.tv_nsec) return 1;
+    else return 0;
+
 }
 
 
 int willExpire(goods g, int quantity, ship s, port startingPort, port destinationPort){
     double totalTime=0;
-    struct timespec expireTime;
+    struct timespec endTravelDate;
     totalTime+=getTravelTime(getDistance(startingPort.coords, s.coords)+getDistance(startingPort.coords, destinationPort.coords));
     totalTime+=2*(quantity*SO_LOADSPEED);
 
-    clock_gettime(CLOCK_REALTIME, &expireTime);
+    clock_gettime(CLOCK_REALTIME, &endTravelDate);
 
-    expireTime.tv_sec += (int)totalTime;
-    if(expireTime.tv_nsec+(totalTime-(int)totalTime)>=1000000000){
-        expireTime.tv_sec++;
-        expireTime.tv_nsec=expireTime.tv_nsec-1000000000+(totalTime-(int)totalTime);
+    endTravelDate.tv_sec += (int)totalTime;
+    if(endTravelDate.tv_nsec+(totalTime-(int)totalTime)>=1000000000){
+        endTravelDate.tv_sec++;
+        endTravelDate.tv_nsec=endTravelDate.tv_nsec-1000000000+(totalTime-(int)totalTime);
     }
 
-    if(expireTime.tv_sec > g.generationTime.tv_sec+g.lifeTime) return 0;
-    else if(expireTime.tv_nsec== g.generationTime.tv_sec+g.lifeTime){
-        if(expireTime.tv_nsec>=g.generationTime.tv_nsec) return 0;
-    }else return 1;
+    if(endTravelDate.tv_sec>g.expiringDate.tv_sec)  return 1;
+    else if (endTravelDate.tv_sec<g.expiringDate.tv_sec) return 0;
+    else if (endTravelDate.tv_nsec> g.expiringDate.tv_nsec) return 1;
+    else return 0;
 
 }
